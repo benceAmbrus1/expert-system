df8f16aa9b4c84986bc84e5bd742b42f3ed8d420
ambrusBence1 <o.g.bence@gmail.com>
2018-02-07 14:37:41 +0100

    first com

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -2,12 +2,7 @@ package com.codecool;
 
 public class Main {
 
-    public String getWelcomeString() {
-        return "Hi!";
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
-        System.out.println(main.getWelcomeString());
+        System.out.println("hi");
     }
 }
