44be83830c37be0725398abdffe888de51221ee9
ambrusBence1 <o.g.bence@gmail.com>
2018-02-07 18:31:59 +0100

    implement loadXmlDocument method

--- src/main/java/com/codecool/XMLParser.java
+++ src/main/java/com/codecool/XMLParser.java
@@ -1,8 +1,31 @@
 package com.codecool;
 
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class XMLParser {
 
-    public void loadXMLDocument(String xmlPath){
+    private List<Fact> facts = new ArrayList<>();
+    private List<Question> questions = new ArrayList<>();
+    private Document doc;
 
+    void loadXmlDocument(String xmlPath) throws Exception {
+        try {
+            File input = new File(xmlPath);
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            doc = documentBuilder.parse(input);
+            doc.getDocumentElement().normalize();
+        } catch (Exception e) {
+            throw new Exception();
+        }
+    }
+
+    Document getDoc() {
+        return doc;
     }
 }
