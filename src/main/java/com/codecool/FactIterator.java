f5d18774535d5e3c5615cd181523c36878abcc2a
ambrusBence1 <o.g.bence@gmail.com>
2018-02-08 11:12:11 +0100

    Iterator methods overrides implemented

--- src/main/java/com/codecool/FactIterator.java
+++ src/main/java/com/codecool/FactIterator.java
@@ -1,8 +1,28 @@
 package com.codecool;
 
 import java.util.Iterator;
+import java.util.List;
 
-public class FactIterator implements Iterator {
+public class FactIterator implements Iterator<Fact> {
 
 
+    private List<Fact> facts;
+    private int current = 0;
+
+    public FactIterator(List<Fact> facts) {
+        this.facts = facts;
+        this.current = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return current < facts.size();
+    }
+
+    @Override
+    public Fact next() {
+        int curr = current;
+        current++;
+        return facts.get(curr);
+    }
 }
