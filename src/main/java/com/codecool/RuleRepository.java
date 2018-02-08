be9157217ce5b5a6a83aea3fe2700d4bda044701
ambrusBence1 <o.g.bence@gmail.com>
2018-02-08 11:52:27 +0100

    Rulerepository implemented

--- src/main/java/com/codecool/RuleRepository.java
+++ src/main/java/com/codecool/RuleRepository.java
@@ -1,8 +1,17 @@
 package com.codecool;
 
+import java.util.*;
+
 public class RuleRepository {
+
+    public List<Question> questions = new ArrayList<>();
 
     public void addQuestion(Question question){
 
+        questions.add(question);
+    }
+
+    public Iterator<Question> getIterator(){
+        return new QuestionIterator(questions);
     }
 }
