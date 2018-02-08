d2ed53bc363c99e9721287c3b84efde139cc4fdf
ambrusBence1 <o.g.bence@gmail.com>
2018-02-08 11:46:11 +0100

    most of the Facts call done

--- src/main/java/com/codecool/Fact.java
+++ src/main/java/com/codecool/Fact.java
@@ -1,11 +1,14 @@
 package com.codecool;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Fact {
 
-    String id;
-    String description;
+    private String id;
+    private String description;
+    private Map<String, Boolean> values = new HashMap<>();
 
     public Fact(String id, String description){
         this.id = id;
@@ -13,18 +16,21 @@ public class Fact {
     }
 
     public Set<String> getIdSet() {
+
         return null;
     }
 
     public void setFactValueById(String id, boolean value) {
-
+        values.put(id, value);
     }
 
     public boolean getValueById(String id) {
-        return true;
+
+        return values.get(id);
     }
 
     public String getDescription() {
+
         return description;
     }
 }
